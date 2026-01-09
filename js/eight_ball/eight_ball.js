be711cf662ecf972b9a5819a7eb438a5400b2c6c
armaanFarooqui <sovvyivan@gmail.com>
2026-01-09 19:44:13 +0100

    added eight_ball project

---
+++ js/eight_ball/eight_ball.js
@@ -0,0 +1,47 @@
+// User name (empty by default)
+let userName = 'Armaan';
+
+// Greet user if name exists
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+// User question
+let userQuestion = 'Will I succeed?';
+
+// Log the question
+console.log(`${userName} asks: ${userQuestion}`)
+
+// Random number from 0–7
+const randomNumber = Math.floor(Math.random() * 8);
+
+// Magic Eight Ball answer
+let eightBall = '';
+
+// Decide response
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+}
+
+// Print result
+console.log(eightBall);
